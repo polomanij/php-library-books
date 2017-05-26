0f6c281beec7de403c989f21d75a6bc7a102e043
polomanij <polomanijmail@gmail.com>
2017-05-26 12:23:09 +0300

    modify

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php
-            require_once "books_array.php";
-            
-            $count = 1;
-            
-            foreach ($books as $key)
-            {
-                echo "{$count}. {$key["name"]}<br/>";
-                echo "<img src='imgs/{$key["img"]}' width='200' height='200'/><br/>";
-                echo "{$key["description"]}<br/>{$key["author"]}<br/>{$key["prise"]}<br/><hr>";
-            }
-        ?>
+        <div class="wrap">
+            <?php
+                require_once "books_array.php";
+
+                $count = 1;
+
+                foreach ($books as $key)
+                {
+                    echo "{$count}. {$key["name"]}<br/>";
+                    echo "<img src='imgs/{$key["img"]}' width='200' height='200'/><br/>";
+                    echo "{$key["description"]}<br/>{$key["author"]}<br/>{$key["prise"]}<br/><hr>";
+                }
+            ?>
+        </div>
     </body>
 </html>
